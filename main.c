438d4d3b338c4d0cb7f4154c49771775cbcfd7e3
Smbat Tonoyan <smbat@easydmarc.us>
2024-09-16 13:04:03 +0400

    some fixes

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    char input[100];
+
+    while (1) {
+        printf("Enter two integers separated by space (or type 'exit' to quit): ");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+
+        if (sscanf(input, "%d %d", &num1, &num2) != 2) {
+            if (strncmp(input, "exit", 4) == 0) {
+                break;
+            }
+            printf("Please enter valid integers.\n");
+            continue;
+        }
+
+        printf("The GCD of %d and %d is: %d\n", num1, num2, gcd(num1, num2));
+    }
+
     return 0;
 }
